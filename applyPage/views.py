6a39b4898c3f7afe81a403fb1a8f808c79591503
Harsh Motwani <harshmotwani@Harshs-MacBook-Pro.local>
2020-01-20 16:43:36 +0530

    created a temperary home page with all students requests

--- applyPage/views.py
+++ applyPage/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from applyPage.models import Department, Student, BTP, Other, Lab, LabRequestEntry, BTPRequestEntry, OthersRequestEntry
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<b>Hello World")
+
+    lab_request_list = LabRequestEntry.objects.order_by('date_sent')
+    other_request_list = OthersRequestEntry.objects.order_by('date_sent')
+    request_dict = {"labRequests": lab_request_list, "otherRequests": other_request_list, "student_roll": 180101028}
+
+    return render(request, 'applyPage.html', context=request_dict)
